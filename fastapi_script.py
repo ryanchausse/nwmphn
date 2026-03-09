aa4371777329356d85033aa95aafd4ce65899bb3
Ryan Chausse <chausse@gmail.com>
2026-03-09 15:12:23 +1100

    Adding initial files for script scaffolding purposes

---
+++ fastapi_script.py
@@ -0,0 +1,9 @@
+import os, json
+from helper_functions import HelperFunctions
+from dotenv import load_dotenv
+
+load_dotenv()
+
+print("Starting...")
+print(HelperFunctions.test_function())
+print("Done.")
---
+++ helper_functions.py
@@ -0,0 +1,10 @@
+from dotenv import load_dotenv
+
+load_dotenv()
+
+class HelperFunctions:
+  def __init__():
+      pass
+
+  def test_function():
+    return "test"
